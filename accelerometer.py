22574e06330d28c1fe53f3bfff3799905d3b198f
Fabien M <f.mathe.pro@outlook.fr>
2024-09-24 09:15:30 +0200

    Avoid circular import with accmeter

--- accelerometer.py
+++ accelerometer.py
@@ -1,11 +1,9 @@
-from robot import BeaconRobot
-
 class Accmeter:
     def __init__(self, prec) -> None:
         self.prec = prec
         self.old_speed = 0
 
-    def get_acc(self, robot:BeaconRobot, dt):
+    def get_acc(self, robot, dt):
         dv = robot.speed - self.old_speed
         acc = dv * dt
         return acc
